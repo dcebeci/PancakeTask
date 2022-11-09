6aadbcd79fe6baf7448d6cf23be978513a46a454
dcebeci <cebecidogukan@gmail.com>
2022-11-10 02:02:53 +0300

    tablo oluşturuldu.

--- pancakeTask/src/app/app.module.ts
+++ pancakeTask/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table'
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSliderModule,
+     BrowserAnimationsModule,
+     MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
