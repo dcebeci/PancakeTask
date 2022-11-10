38f4a8c5199f9f2aa2491e0f3b251b92a99dd744
dcebeci <cebecidogukan@gmail.com>
2022-11-10 23:41:04 +0300

    pop up componentle bağlantı yapıldı.

--- pancakeTask/src/app/pop-up/pop-up.component.ts
+++ pancakeTask/src/app/pop-up/pop-up.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Inject} from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-pop-up',
   templateUrl: './pop-up.component.html',
   styleUrls: ['./pop-up.component.css']
 })
-export class PopUpComponent implements OnInit {
 
-  constructor() { }
+export class PopUpComponent implements OnInit {
+ 
+  constructor(@Inject(MAT_DIALOG_DATA) public data) {
+    
+
+   }
 
   ngOnInit(): void {
   }
